7a17039cef75546ec503de079ffabaf70c30ee7f
Thomas Petchel <tpetchel@gmail.com>
2018-02-01 22:24:34 -0500

    Resolve warnings

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,17 +2,12 @@
 # Cookbook Name:: custom_apache
 # Recipe:: default
 #
-# Copyright (c) 2015 The Authors, All Rights Reserved.
-
-#################################################
-# This demonstrates CHEF-4: Some Attribute Methods.
-# https://docs.chef.io/deprecations_attributes.html
-#################################################
+# Copyright (c) 2015-2018 The Authors, All Rights Reserved.
 
 # Load from node attributes some variables we'll need.
-site_name = node.site.name
-content_owner = node.site.content.owner
-content_group = node.site.content.group
+site_name = node['site']['name']
+content_owner = node['site']['content']['owner']
+content_group = node['site']['content']['group']
 
 # Ensure the apt cache is up-to-date.
 apt_update 'Update the apt cache daily' do
@@ -20,39 +15,20 @@ apt_update 'Update the apt cache daily' do
   action :periodic
 end
 
-#################################################
-# This demonstrates CHEF-3694: resource cloning
-# https://docs.chef.io/deprecations_resource_cloning.html
-#################################################
-
-# Install additional Apache packages.
-packages = %w( libapache2-modsecurity libapache2-mod-spamhaus )
-
-packages.each do |p|
-  package 'install libapache2 package' do
-    package_name p
-  end
-end
+package %w( libapache2-modsecurity libapache2-mod-spamhaus )
 
 # Create the group that owns web content.
 group content_group do
   action :create
 end
 
-#################################################
-# This demonstrates CHEF-8: "Supports" metaproperty
-# https://docs.chef.io/deprecations_supports_property.html
-#################################################
-
 # Create the user that owns web content.
 user content_owner do
   group content_group
   home "/home/#{content_owner}"
   shell '/bin/bash'
-  supports({
-    manage_home: true,
-    non_unique: false
-  })
+  manage_home true
+  non_unique false
 end
 
 # Use the custom site resource to configure the website.
